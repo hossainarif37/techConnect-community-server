1d8cc18592a2ba973830bbc5dad2fbff7e9a41b8
hossainarif37 <hossainarif4937@gmail.com>
2023-10-05 12:01:28 +0600

    docs: Add API Documentation for user registration route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,20 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
 router
+    //* User Registration Route
+    /**
+     * @route POST /api/auth/register
+     * @description Register a new user.
+     * @access Public
+     * 
+     * @params name, email, password
+     * 
+     * @returns {object} - Registration status and message
+     * 
+     * @throws {409} If the user already exist
+     * @throws {400} If there are validation errors in the request body
+     * @throws {500} If there's an Internal server error
+     */
     .post('/register', authController.registerUser)
 
 
