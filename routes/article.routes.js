d2bf4273f361b1b81d6faabe9fc505583ddcd42a
hossainarif37 <hossainarif4937@gmail.com>
2023-10-06 11:45:23 +0600

    docs: Update article api docs with middleware docs

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -5,21 +5,28 @@ const { isAuthenticateUser } = require('../middleware/authorization');
 
 router
     //* Creates a new article
+    //* Route
     /**
- * @route POST /api/article
- * @description Create a new article.
- * @access Public
- *
- * @param {string} title - The title of the article (required).
- * @param {string} content - The content of the article (required).
- * @param {string} category - The category to which the article belongs (required).
- * @param {string} author - The ID of the author who created the article (required).
- *
- * @returns {object} - Registration status and message
- *
- * @throws {400} If required fields (`title`, `content`, `category` or `author`) are missing.
- * @throws {500} If there's an Internal Server Error.
- */
+     * @route POST /api/article
+     * @description Create a new article.
+     * @access Public
+     *
+     * @param {string} title - The title of the article (required).
+     * @param {string} content - The content of the article (required).
+     * @param {string} category - The category to which the article belongs (required).
+     * @param {string} author - The ID of the author who created the article (required).
+     *
+     * @returns {object} - Registration status and message
+     *
+     * @throws {400} If required fields (`title`, `content`, `category` or `author`) are missing.
+     * @throws {500} If there's an Internal Server Error.
+    */
+    //* Middleware
+    /**
+     * @middleware isAuthenticateUser
+     * @description Check if the user is authenticated before allowing access to certain routes.
+     * @access Private
+    */
     .post('/', isAuthenticateUser, articleController.createArticle)
 
     //* Get all articles
