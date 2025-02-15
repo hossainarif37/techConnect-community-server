df8f9073d60237fb9c19fe99b536611e71930471
hossainarif37 <hossainarif4937@gmail.com>
2025-02-16 01:02:17 +0600

    refactor: improve comment deletion logic and response handling

--- controllers/comment.controller.ts
+++ controllers/comment.controller.ts
@@ -13,7 +13,7 @@ exports.createComment = async (req: Request, res: Response, next: NextFunction) 
         const articleId = req.body.article;
         await Article.findByIdAndUpdate(articleId, { $push: { comments: newComment._id } });
 
-        res.status(201).json({ success: true, comment: response})
+        res.status(201).json({ success: true, comment: response })
     } catch (error) {
         console.log('Create Comment Controller: ', (error as Error).message);
         next(error)
@@ -115,37 +115,37 @@ exports.deleteComment = async (req: Request, res: Response, next: NextFunction) 
 
         // Find the comment to confirm the author
         const comment = await Comment.findById(commentId);
-        
+
         // Check if the comment exists
         if (!comment) {
-            return res.status(404).json({success: false, message: "Comment not found" });
+            return res.status(404).json({ success: false, message: "Comment not found" });
         }
 
         // Check if the user is the author of the comment or the article author
         const article = await Article.findById(articleId);
         if (!article) {
-            return res.status(404).json({success: false, message: "Article not found" });
+            return res.status(404).json({ success: false, message: "Article not found" });
         }
 
         const isArticleAuthor = article.author.toString() === userId.toString();
         const isCommentAuthor = comment.author.toString() === userId.toString();
 
-        if (!isArticleAuthor || !isCommentAuthor) {
-            return res.status(403).json({success: false, message: "You are not authorized to delete this comment" });
+
+        if (isArticleAuthor || isCommentAuthor) {
+            // Delete the comment
+            await Comment.findByIdAndDelete(commentId);
+
+            // Remove the comment reference from the article's comments array
+            article.comments = article.comments.filter(
+                (comment) => comment.toString() !== commentId
+            );
+            await article.save();
+
+            // Return success response
+            return res.status(200).json({ success: true, message: "Comment deleted successfully" });
+        } else {
+            return res.status(403).json({ success: false, message: "You are not authorized to delete this comment" });
         }
-
-        // Delete the comment
-        await Comment.findByIdAndDelete(commentId);
-
-        // Remove the comment reference from the article's comments array
-        article.comments = article.comments.filter(
-            (comment) => comment.toString() !== commentId
-        );
-        await article.save();
-
-        // Return success response
-        return res.status(200).json({ success: true, message: "Comment deleted successfully" });
-
     } catch (error) {
         console.error("Error deleting comment:", error);
         next(error);
